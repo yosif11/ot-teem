bfb9fef1b1539e463b48408806ccf9f8c359150f
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-08-05 08:11:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+const express = require("express");
+const app = express();
 
+app.listen(() => console.log("Server started"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+const Discord = require('discord.js');
+const cmd = require("node-cmd");
+const ms = require("ms");
+const fs = require('fs');
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const simpleytapi = require('simple-youtube-api')
+const util = require("util")
+const gif = require("gif-search");
+const jimp = require("jimp");
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require("pretty-ms");
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+
+////////////
+
+
+
+
+client.login(process.env.token1);
+client2.login(process.env.token2);
+client3.login(process.env.token3);
+client4.login(process.env.token4);
+client5.login(process.env.token5);
+client6.login(process.env.token6);
+client7.login(process.env.token7);
+client8.login(process.env.token8);
+client9.login(process.env.token9);
+client10.login(process.env.token10);
